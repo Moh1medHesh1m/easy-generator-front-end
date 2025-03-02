439584605ff3acca5cb8a4c79258cbfa963a63c6
Mohamed Hesham <mohamedbelal814@yahoo.com>
2025-03-02 17:55:40 +0200

    fix: pipeline

--- src/lib/requests/constants.ts
+++ src/lib/requests/constants.ts
@@ -2,4 +2,4 @@ import axios from 'axios';
 
 import { config } from '../../utils/config';
 
-export const apiClient = axios.create({ baseURL :"https://easy-generator-api.onrender.com/api" });
+export const apiClient = axios.create({ baseURL : config.api ?? "https://easy-generator-api.onrender.com/api" });
