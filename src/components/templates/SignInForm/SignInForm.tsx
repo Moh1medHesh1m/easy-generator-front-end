ea84db85f69ed5883ddc7b194e260e1f843034df
Mohamed Hesham <mohamedbelal814@yahoo.com>
2025-03-04 19:33:27 +0200

    fix: disabled btn during pending requests

--- src/components/templates/SignInForm/SignInForm.tsx
+++ src/components/templates/SignInForm/SignInForm.tsx
@@ -1,28 +1,28 @@
-'use client';
+"use client";
 
-import { yupResolver } from '@hookform/resolvers/yup';
-import { Button } from 'flowbite-react';
-import Link from 'next/link';
-import { useRouter } from 'next/navigation';
-import React from 'react';
-import { FormProvider, useForm } from 'react-hook-form';
+import { yupResolver } from "@hookform/resolvers/yup";
+import { Button } from "flowbite-react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import { FormProvider, useForm } from "react-hook-form";
 
-import EmailInputField from '@/components/atoms/EmailInputField/EmailInputField';
-import PasswordInputField from '@/components/atoms/PasswordInputField/PasswordInputField';
-import { useAppDispatch } from '@/lib/hooks';
-import { authThunks } from '@/lib/redux/features/auth.slice';
-import type { IError } from '@/lib/requests/errors';
-import type { SignInFormData } from '@/lib/types/auth.type';
-import { SignInFormValidator } from '@/lib/validators/auth.validator';
+import EmailInputField from "@/components/atoms/EmailInputField/EmailInputField";
+import PasswordInputField from "@/components/atoms/PasswordInputField/PasswordInputField";
+import { useAppDispatch } from "@/lib/hooks";
+import { authThunks } from "@/lib/redux/features/auth.slice";
+import type { IError } from "@/lib/requests/errors";
+import type { SignInFormData } from "@/lib/types/auth.type";
+import { SignInFormValidator } from "@/lib/validators/auth.validator";
 
 export default function SignInForm() {
   const dispatch = useAppDispatch();
   const methods = useForm<SignInFormData>({
-    mode: 'onSubmit',
+    mode: "onSubmit",
     resolver: yupResolver(SignInFormValidator),
     defaultValues: {
-      email : '',
-      password: '',
+      email: "",
+      password: "",
     },
   });
   const {
@@ -31,33 +31,43 @@ export default function SignInForm() {
     formState: { errors },
   } = methods;
   const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
   const onSubmit = (data: SignInFormData) => {
+    setIsLoading(true);
+
     dispatch(authThunks.signIn(data))
       .then(() => {
-        router.push('/home');
+        router.push("/home");
+        setIsLoading(false);
       })
-      .catch((err: IError) => err.handle());
+      .catch((err: IError) => err.handle())
+      .finally(() => {
+        setIsLoading(false);
+      });
   };
   return (
     <div>
       <FormProvider {...methods}>
         <form className="flex flex-col gap-4" onSubmit={handleSubmit(onSubmit)}>
           <EmailInputField
-            register={register('email')}
+            register={register("email")}
             labelText="Email"
             placeholder="example@domain.com"
             errorMessage={errors.email?.message}
           />
           <PasswordInputField
-            register={register('password')}
+            register={register("password")}
             labelText="Password"
             errorMessage={errors.password?.message}
           />
-          <Button type="submit">Submit</Button>
+          <Button type="submit" disabled={isLoading}>
+            { "Submit" }
+          </Button>{" "}
         </form>
       </FormProvider>
       <div className="flex justify-between">
-        <Link href={'/auth/register'}> Create new account</Link>
+        <Link href={"/auth/register"}> Create new account</Link>
       </div>
     </div>
   );
--- src/components/templates/SignUpForm/SignUpForm.tsx
+++ src/components/templates/SignUpForm/SignUpForm.tsx
@@ -1,31 +1,32 @@
-'use client';
+"use client";
 
-import { yupResolver } from '@hookform/resolvers/yup';
-import { Button } from 'flowbite-react';
-import { useRouter } from 'next/navigation';
-import React from 'react';
-import { FormProvider, useForm } from 'react-hook-form';
+import { yupResolver } from "@hookform/resolvers/yup";
+import { Button } from "flowbite-react";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import { FormProvider, useForm } from "react-hook-form";
 
-import EmailInputField from '@/components/atoms/EmailInputField/EmailInputField';
-import TextInputField from '@/components/atoms/InputTextField/InputTextField';
-import PasswordInputField from '@/components/atoms/PasswordInputField/PasswordInputField';
-import { useAppDispatch } from '@/lib/hooks';
-import { authThunks } from '@/lib/redux/features/auth.slice';
-import type { IError } from '@/lib/requests/errors';
-import type { SignUpFormData } from '@/lib/types/auth.type';
-import { SignUpValidator } from '@/lib/validators/auth.validator';
+import EmailInputField from "@/components/atoms/EmailInputField/EmailInputField";
+import TextInputField from "@/components/atoms/InputTextField/InputTextField";
+import PasswordInputField from "@/components/atoms/PasswordInputField/PasswordInputField";
+import { useAppDispatch } from "@/lib/hooks";
+import { authThunks } from "@/lib/redux/features/auth.slice";
+import type { IError } from "@/lib/requests/errors";
+import type { SignUpFormData } from "@/lib/types/auth.type";
+import { SignUpValidator } from "@/lib/validators/auth.validator";
 
 export default function SignUpForm() {
   const dispatch = useAppDispatch();
+  const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
   const methods = useForm<SignUpFormData>({
-    mode: 'onSubmit',
+    mode: "onSubmit",
     resolver: yupResolver(SignUpValidator),
     defaultValues: {
-      name: '',
-      email: '',
-      password: '',
-      confirmPassword: '',
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
     },
   });
   const {
@@ -35,40 +36,47 @@ export default function SignUpForm() {
   } = methods;
 
   const onSubmit = (data: SignUpFormData) => {
+    setIsLoading(true);
     dispatch(authThunks.signUp(data))
       .then(() => {
-        router.push('/auth/login');
+        router.push("/auth/login");
+        setIsLoading(false);
+
       })
-      .catch((err: IError) => err.handle());
+      .catch((err: IError) => err.handle())
+      .finally(() => {
+        setIsLoading(false);
+      });
   };
   return (
     <div>
       <FormProvider {...methods}>
         <form className="flex flex-col gap-4" onSubmit={handleSubmit(onSubmit)}>
           <TextInputField
-            register={register('name')}
+            register={register("name")}
             labelText="Full Name"
             placeholder="John Doe"
             errorMessage={errors.name?.message}
           />
           <EmailInputField
-            register={register('email')}
+            register={register("email")}
             labelText="Email"
             placeholder="example@domain.com"
             errorMessage={errors.email?.message}
           />
           <PasswordInputField
-            register={register('password')}
+            register={register("password")}
             labelText="Password"
             errorMessage={errors.password?.message}
           />
           <PasswordInputField
-            register={register('confirmPassword')}
+            register={register("confirmPassword")}
             labelText="Confirm Password"
             errorMessage={errors.confirmPassword?.message}
           />
-       
-          <Button type="submit">Submit</Button>
+          <Button type="submit" disabled={isLoading}>
+            {isLoading ? "Submitting..." : "Submit"}
+          </Button>{" "}
         </form>
       </FormProvider>
     </div>
